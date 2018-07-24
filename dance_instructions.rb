d3a956d3e0c80169b2d54846ad4a24a2566ecbcf
Siri-Dandu <sdandu99@gmail.com>
2018-07-24 15:15:51 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -14,7 +14,9 @@ def base_footwork
 end 
 
 def skip_step(starting_foot)
-  puts "Skip a step (lower, bounce, briefly)"
+   puts "Lower #{starting_foot} to the ground"
+	  puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
+	  puts "Briefly raise the opposite foot, then the #{starting_foot} twice more!"
 end
 
 
